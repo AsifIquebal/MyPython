56a454492b414d4405638e46020e897ccdda2145
asif <asu9421@gmail.com>
2019-06-06 22:48:26 +0530

    r&d request session

--- WebService/pytestapi.py
+++ WebService/pytestapi.py
@@ -1,15 +1,44 @@
+import jsonpath
 import pytest
 import requests
 
 
+@pytest.fixture
+def get_auth_header():
+    headers_payload = {"content-type": "application/x-www-form-urlencoded",
+                       "Authorization": "Basic Y2xpZW50YXBwOjEyMzQ1Ng=="}
+    payload = {'username': 'liveaug29@indition.net',
+               'password': 'Password!1',
+               "grant_type": "password",
+               "scope": "write",
+               "app": "crmsales"}
+    r = requests.post(("https://api.inditioncrm.com/CrmSalesTeam-1.28/oauth/token"),
+                      headers=headers_payload,
+                      params=payload,
+                      verify=False)
+    # l = jsonpath_ng.parse(r.json(),"$.access_token")
+    l = jsonpath.jsonpath(r.json(), "$.access_token")
+    # return l[0]
+    yield {"Authorization": f"Bearer {l[0]}"}
 
+
+def test_token(get_auth_header):
+    print(get_auth_header)
+
+
+def test01_get_number_of_companies(get_auth_header):
+    response = requests.get(("https://api.inditioncrm.com/CrmSalesTeam-1.28/picklist/countries"),
+                            headers=get_auth_header,
+                            verify=False
+                            )
+    print("Total Countries found: ", len(jsonpath.jsonpath(response.json(), "$.[*].id")))
 
 def test_01():
     response = requests.get('https://api.github.com')
     print("Status Code: ",response.status_code)
 
-def test_02():
 
+def test_02():
     # Search GitHub's repositories for requests
     response = requests.get(
         'https://api.github.com/search/repositories',
@@ -21,3 +50,12 @@ def test_02():
     repository = json_response['items'][0]
     print(f'Repository name: {repository["name"]}')  # Python 3.6+
     print(f'Repository description: {repository["description"]}')  # Python 3.6+
+
+def test_03():
+    r = requests.head('https://httpbin.org/get')
+    print(r)
+
+
+def test_04():
+    r = requests.options('https://httpbin.org/get')
+    print(r.text)
--- WebService/usingSession.py
+++ WebService/usingSession.py
@@ -1,40 +1,9 @@
 import jsonpath
 import pytest
 import requests
+from requests import Session
 
-
-@pytest.fixture
-def get_auth_header():
-    headers_payload = {"content-type": "application/x-www-form-urlencoded",
-                       "Authorization": "Basic Y2xpZW50YXBwOjEyMzQ1Ng=="}
-    payload = {'username': 'liveaug29@indition.net',
-               'password': 'Password!1',
-               "grant_type": "password",
-               "scope": "write",
-               "app": "crmsales"}
-    r = requests.post(("https://api.inditioncrm.com/CrmSalesTeam-1.28/oauth/token"),
-                      headers=headers_payload,
-                      params=payload,
-                      verify=False)
-    # l = jsonpath_ng.parse(r.json(),"$.access_token")
-    l = jsonpath.jsonpath(r.json(), "$.access_token")
-    # return l[0]
-    yield {"Authorization": f"Bearer {l[0]}"}
-
-
-def test_token(get_auth_header):
-    print(get_auth_header)
-
-
-def test01_get_number_of_companies(get_auth_header):
-    response = requests.get(("https://api.inditioncrm.com/CrmSalesTeam-1.28/picklist/countries"),
-                            headers=get_auth_header,
-                            verify=False
-                            )
-    print("Total Countries found: ", len(jsonpath.jsonpath(response.json(), "$.[*].id")))
-
-
-ssn = requests.session()
+ssn: Session = requests.session()
 
 
 @pytest.fixture
@@ -61,7 +30,7 @@ def test01_session(set_session):
                        verify=False
                        )
     print("Total Countries found: ", len(jsonpath.jsonpath(response.json(), "$.[*].id")))
-    
+
 # def test02_oo():
 #     header = {"Content-Type": "application/json;charset=UTF-8",
 #               "Authorization": f"Bearer {get_access_token()}"}
